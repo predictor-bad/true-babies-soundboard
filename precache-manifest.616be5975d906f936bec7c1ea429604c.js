fd01d29859c7c7a6ef064f7a76e887659bd6282a
wes <wes@computer.local>
2019-07-15 19:41:59 -0700

    Updates

---
+++ precache-manifest.616be5975d906f936bec7c1ea429604c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f619cc4286ab4f8288c3aec097d32f27",
+    "url": "/true-babies-soundboard/index.html"
+  },
+  {
+    "revision": "9217f64cb2c807fb8bfb",
+    "url": "/true-babies-soundboard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/true-babies-soundboard/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9217f64cb2c807fb8bfb",
+    "url": "/true-babies-soundboard/static/js/main.cd6f3865.chunk.js"
+  },
+  {
+    "revision": "dee3d6e8793c4e71c109",
+    "url": "/true-babies-soundboard/static/js/runtime~main.49baa5ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/true-babies-soundboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
